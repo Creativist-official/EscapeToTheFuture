a34d1f6b62d6426f263f188eb1da0bf1bda8572c
Andrea Losacco <losaccoandrea04@gmail.com>
2025-02-28 16:34:57 +0100

    Creata splash-sceen
    aggiunte anche animazioni nel css generale

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ label, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-[url(../images/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat w-auto text-black font-bold text-3xl py-8 px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform"
+      className="z-1 bg-[url(../images/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat w-auto text-black font-bold text-2xl xl:text-6xl py-6 xl:py-10 px-12 xl:px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform"
     >
       {label}
     </button>
---
+++ src/schools/Bitritto/SplashScreen.jsx
@@ -0,0 +1,39 @@
+import Button from "../../components/Button";
+import ampolla from "../../assets/images/ampolla.png";
+import { useEffect } from "react";
+
+const SplashScreen = ({ title, location }) => {
+
+    useEffect(() => {
+        // Disabilita lo scroll della pagina
+        document.body.style.overflow = "hidden";
+
+            // Ripristina lo scroll quando il componente viene smontato
+    return () => {
+        document.body.style.overflow = "auto";
+      };
+    }, []);
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-2 md:gap-10 h-screen bg-[url(../images/bg-splash.webp)] bg-center bg-clip-border bg-cover bg-origin-border bg-no-repeat gap-auto">
+      <div className="absolute inset-0 bg-black opacity-60"></div>
+
+      <h1 className="2xl:w-1/2 md:w-2/3 w-2/3 text-5xl md:text-7xl xl:text-8xl font-bold text-center font-elite text-orange-600 z-1">
+        {title}
+      </h1>
+      <div className="flex items-end justify-center gap-1">
+        <h3 className="font-elite text-3xl md:text-3xl xl:text-6xl z-1 text-white">
+          {location}
+        </h3>
+        <img
+          src={ampolla}
+          alt="ampolla"
+          className="h-10 xl:h-16 z-1 transition-transform duration-300 ease-in-out transform hover:scale-120 animate-shake"
+        />
+      </div>
+      <Button label="INIZIA" onClick={() => props.history.push("/bitritto")} />
+    </div>
+  );
+};
+
+export default SplashScreen;
