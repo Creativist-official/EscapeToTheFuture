9b3ce3b8bca59aa0c7b68de6d3cf3dcb006abfd4
Jonnycp <jonathan-caputo@hotmail.com>
2025-03-07 20:49:27 +0100

    Rotate phone e gestione gif

--- src/Bitritto.jsx
+++ src/Bitritto.jsx
@@ -1,14 +1,36 @@
 import SplashScreen from "@components/SplashScreen";
 import { MemoryRouter, Routes, Route } from "react-router";
+import RotatePhone from "./components/RotatePhone";
+import { useEffect, useState } from "react";
 
 const Bitritto = () => {
-  return (
-    <MemoryRouter>
-      <Routes>
-        <Route path="/" element={<SplashScreen location={'SOLE LUNA BITRITTO'} title={'IL LABORATORIO DEL MAGO PDOR'} />} />
+  const [isPortrait, setIsPortrait] = useState(window.matchMedia("(orientation: portrait)").matches);
 
-      </Routes>
-    </MemoryRouter>
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      setIsPortrait(window.matchMedia("(orientation: portrait)").matches);
+    };
+
+    window.addEventListener("resize", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("resize", handleOrientationChange);
+    };
+  }, []);
+
+  
+  return (
+    <>
+      {isPortrait ? (
+        <RotatePhone />
+      ) : (
+        <MemoryRouter>
+          <Routes>
+            <Route path="/" element={<SplashScreen location={'SOLE LUNA BITRITTO'} title={'IL LABORATORIO DEL MAGO PDOR'} />} />
+          </Routes>
+        </MemoryRouter>
+      )}
+    </>
   );
 };
 
