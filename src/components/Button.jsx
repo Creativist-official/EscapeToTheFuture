a9a9198460dff26ef6be58e18df14c127c4a6f79
Jonnycp <jonathan-caputo@hotmail.com>
2025-03-07 20:49:07 +0100

    select-none e h-svh

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Button = ({ label, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="z-1 bg-[url(../images/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat w-auto text-black font-bold text-2xl xl:text-6xl py-6 xl:py-10 px-12 xl:px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform"
+      className="z-1 bg-[url(../images/generic/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat select-none text-black font-bold text-2xl xl:text-6xl py-6 xl:py-10 px-12 xl:px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform"
     >
       {label}
     </button>
--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -2,25 +2,34 @@ import Button from "@components/Button";
 import ampolla from "@assets/images/ampolla.png";
 
 const SplashScreen = ({ title, location }) => {
-
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-2 md:gap-10 h-screen bg-[url(../images/bg-splash.webp)] bg-center bg-clip-border bg-cover bg-origin-border bg-no-repeat gap-auto">
+    <div className="w-full flex flex-col items-center justify-center gap-2 md:gap-10 h-svh bg-[url(../images/bg-splash.webp)] bg-center bg-clip-border bg-cover bg-origin-border bg-no-repeat gap-auto">
       <div className="absolute inset-0 bg-black opacity-60"></div>
 
-      <h1 className="2xl:w-1/2 md:w-2/3 w-2/3 text-5xl md:text-7xl xl:text-8xl font-bold text-center font-elite text-orange-600 z-1">
+      <h1 className="2xl:w-1/2 md:w-2/3 w-2/3 text-5xl md:text-7xl xl:text-8xl font-bold text-center select-none font-elite text-orange-600 z-1">
         {title}
       </h1>
       <div className="flex items-end justify-center gap-1">
-        <h3 className="font-elite text-3xl md:text-3xl xl:text-6xl z-1 text-white">
+        <h3 className="font-elite text-3xl md:text-3xl xl:text-6xl z-1 text-white select-none">
           {location}
         </h3>
         <img
           src={ampolla}
           alt="ampolla"
-          className="h-10 xl:h-16 z-1 transition-transform duration-300 ease-in-out transform hover:scale-120 animate-shake"
+          className="select-none h-10 xl:h-16 z-1 transition-transform duration-300 ease-in-out transform hover:scale-120 animate-shake"
         />
       </div>
-      <Button label="INIZIA" onClick={() => props.history.push("/bitritto")} />
+      <Button
+        label="INIZIA"
+        onClick={async () => {
+        console.log(document.fullscreenElement)
+          try {
+            await document.body.requestFullscreen();
+          } catch (err) {
+            console.error(err.name, err.message);
+          }
+        }}
+      />
     </div>
   );
 };
