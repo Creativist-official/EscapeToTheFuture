456e6816c9b561cdf6f120859d9199bc027e1295
Andrea Losacco <losaccoandrea04@gmail.com>
2025-03-11 13:57:24 +0100

    Creato Scena 1 da completare

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Button = ({ label, onClick }) => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
   return (
     <button
       onClick={onClick}
-      className="z-1 bg-[url(../images/generic/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat select-none text-black font-bold text-2xl xl:text-6xl py-6 xl:py-10 px-12 xl:px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform"
+      className={`z-1 bg-[url(../images/generic/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat select-none text-black font-bold text-2xl xl:text-4xl py-6 xl:py-10 px-12 xl:px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform ${
+        isLoaded ? "opacity-100" : "opacity-0"
+      }  duration-1000`}
     >
       {label}
     </button>
--- src/scenes/Scena1.jsx
+++ src/scenes/Scena1.jsx
@@ -1,7 +1,62 @@
+import Button from "../components/Button";
+import letteraC from "@assets/images/Lettera_C.webp";
+import { useState } from "react";
+import letteraO from "@assets/images/Lettera_O.webp";
+
 const Scena1 = () => {
+  const [imageSrc, setImageSrc] = useState(letteraC);
+  const [buttonVisible, setButtonVisible] = useState(true);
+  const [divVisible, setDivVisible] = useState(false);
+
+  const handleClick = () => {
+    setImageSrc(letteraO);
+    setButtonVisible(false);
+    setDivVisible(true);
+  };
+
   return (
-    <div>
-      <h1>Scena 1</h1>
+    <div className="w-screen h-svh bg-black flex flex-col items-center justify-center bg-[url(../images/Legno.webp)] bg-center bg-cover bg-no-repeat bg-opacity-50 gap-10">
+      {buttonVisible && (
+        <Button label="Apri la lettera" onClick={handleClick} />
+      )}
+      <div className="w-5/11 relative">
+        <img
+          src={imageSrc}
+          alt="Lettera"
+          className="w-full mb-40 transform transition-transform duration-2000 ease-in-out"
+          style={{ transform: "scale(0) rotate(0deg)" }}
+          onLoad={(e) => {
+            e.target.style.transform = "scale(1) rotate(360deg)";
+          }}
+          onClick={handleClick}
+        />
+        {divVisible && (
+          <>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <div
+                className={`absolute bg-gray-200 font-schibsted flex flex-col items-center justify-center p-6 gap-2 w-3/4 rounded-sm transition-opacity duration-1000 ease-in-out ${
+                  divVisible ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                <h3 className="xl:text-2xl text-sm font-bold text-black text-center">
+                  Ciao, ti scrivo di nascosto questa lettera per aiutarmi a
+                  liberare Enrico.
+                </h3>
+                <p className="xl:text-xl text-xs font-semibold text-blue-800 text-center">
+                  Troverai tutte le indicazioni necessarie per trovare il
+                  laboratorio dove è prigioniero.
+                </p>
+              </div>
+            </div>
+            <div className="absolute bottom-40 flex justify-center items-center">
+              {/* Pulsante "Chiudi lettera" a sinistra */}
+              <Button label="Chiudi lettera" onClick={() => {}} />
+              {/* Pulsante "Esamina meglio" a destra */}
+              <Button label="Esamina meglio" onClick={() => {}} />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
