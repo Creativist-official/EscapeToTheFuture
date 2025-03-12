4e9ec2c5bea56b4beac9922c8d79d46d3330835e
Jonnycp <jonathan-caputo@hotmail.com>
2025-03-12 16:11:20 +0100

    Modifiche a button per renderlo più generico

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({ label, onClick }) => {
+const Button = ({ children, onClick, classes, noAnimation=false }) => {
   const [isLoaded, setIsLoaded] = React.useState(false);
 
   React.useEffect(() => {
@@ -10,11 +10,12 @@ const Button = ({ label, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`z-1 bg-[url(../images/generic/bg-button.png)] font-elite w-fit flex bg-center bg-contain bg-no-repeat select-none text-black font-bold text-2xl xl:text-4xl py-6 xl:py-10 px-12 xl:px-20 hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform ${
+      style={{backgroundSize: "100% 82px"}}
+      className={`z-1 bg-[url(../images/generic/bg-button.png)] font-elite w-fit flex bg-center bg-no-repeat select-none text-black font-bold text-2xl xl:text-4xl py-6 xl:py-10 px-12 xl:px-20 ${!noAnimation && "hover:cursor-pointer hover:-rotate-5 hover:scale-110 duration-600 hover:transition-transform"} ${
         isLoaded ? "opacity-100" : "opacity-0"
-      }  duration-1000`}
+      } ${classes} duration-1000`}
     >
-      {label}
+      {children}
     </button>
   );
 };
