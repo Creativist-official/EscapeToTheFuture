9a86c195361a4c6cfbbeab7642547a6445b33cee
T_Ferrarah <55454285+TFerrarah@users.noreply.github.com>
2025-03-22 18:12:43 +0100

    Aggiunto preload e interazione con televisione

--- src/scenes/Scena7.jsx
+++ src/scenes/Scena7.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import ImageMapper from "react-img-mapper";
 
 import Camera from '@assets/images/Scena7/Camera.png';
 import Notiziario from '@assets/images/Scena7/Notiziario.png';
@@ -12,6 +13,12 @@ const Scena7 = () => {
     const [currentImage, setCurrentImage] = useState(Camera);
     const [scene, setScene] = useState(sceneBitritto.scenes[13]);
 
+    // Preload
+    const [preload, setPreload] = useState({
+        camera: false,
+        notiziario: false,
+    });
+
     return (
         <div>
             {/* Dialogues */}
@@ -24,26 +31,78 @@ const Scena7 = () => {
                                 setCurrentDialogueIndex(currentDialogueIndex + 1);
                             } else {
                                 console.log('All dialogues are finished');
-                                // if scene index is 13 go to 14
-                                if (scene === sceneBitritto.scenes[13]) {                                    
-                                    setScene(sceneBitritto.scenes[14]);
-                                    setCurrentDialogueIndex(0);
-                                    // Change image to Notiziario
-                                    setCurrentImage(Notiziario);
-                                }
-
                             }
                         }} />
                     )
                 ))
             }
-            <div className="flex flex-col justify-center items-center h-svh">
-                <img
+            
+            {/* ImageMapper sfondo */}
+            <div className="z-0 flex flex-col justify-center items-center h-svh">
+                {/* Preload */}
+                {
+                    !preload.camera &&
+                    <img
+                        src={Camera}
+                        alt="Cucina locked"
+                        className={'img-mapper-img'}
+                        style={
+                            {
+                                position: 'absolute',
+                                top: '0px',
+                                left: '0px',
+                                zIndex: '1',
+                                userSelect: 'none',
+                                width: '100%',
+                                height: 'auto',
+                            }
+                        }
+                    />
+                }
+
+                <ImageMapper
                     src={currentImage}
-                    alt="Ingresso cucina"
-                    className='w-full h-full object-cover'
+                    name="Cucina"
+                    natural
+                    // Preload
+                    onLoad={() => {
+                        if (!preload.camera) {
+                            setPreload({
+                                ...preload,
+                                camera: true,
+                            });
+                        }
+                    }}
+                    imgWidth={1920}
+                    parentWidth={window.innerWidth > 1920 ? 1920 : window.innerWidth}
+                    responsive={true}
+                    disabled={currentDialogueIndex < scene.dialogue.length-1}
+                    areas={[
+                    {
+                        id: "televisione",
+                        shape: "rect",
+                        coords: [380, 310, 1050, 690],
+                        fillColor: "rgba(237, 20, 61, 0.5)",
+                        lineWidth: 0,
+                        strokeColor: "rgba(237, 20, 61, 0.5)",
+                        disabled: currentImage === Notiziario,
+                    }
+                    ]}
+                    onClick={(area) => {
+                        if (area.id === "televisione") {
+                            console.log('Clicked on televisione');
+                            // Change image to Notiziario
+                            setCurrentImage(Notiziario);
+                            // Change scene to 14
+                            setScene(sceneBitritto.scenes[14]);
+                            // Reset dialogues
+                            setCurrentDialogueIndex(0);
+                        }
+                    }}
+                    isMulti={false}
                 />
             </div>
+
         </div>
     );
 };
